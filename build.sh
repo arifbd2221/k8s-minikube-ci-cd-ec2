fb20b1b3042c6bfeebde8c5059a3d4d79ecbe26e
arif <mohidul.haque@goama.com>
2024-09-23 02:06:00 +0600

    Added log to debug the image tagging issue

--- build.sh
+++ build.sh
@@ -3,8 +3,15 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Log the current directory and list files
+echo "Current directory: $(pwd)"
+ls -al
+
 # Get the last commit count as the version
 VERSION=$(git rev-list --count HEAD)
+
+# Log the version
+echo "Version tag: $VERSION"
 
 # Build the Docker image with the commit count as the version tag
 docker build -t "$DOCKER_USERNAME/$IMAGE_NAME:$VERSION" .
